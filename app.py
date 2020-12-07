5da1cf292003e95b91ca6f940e00927a6b0dd7b9
jaminiow <75037391+jaminiow@users.noreply.github.com>
2020-12-07 08:44:49 -0600

    Update app.py

--- app.py
+++ app.py
@@ -2,34 +2,15 @@
 import streamlit as st
 st.set_page_config(layout="wide")
 
-import app1
+#import app1
 #import app2
 #import app3
 #import app4
 import streamlit as st
 import numpy as np
 import pandas as pd  #Used for Dataframe Creations
-#import pyodbc as py  #Used for Database Connections
-#import openpyxl  # Used for Excel
-#import sqlalchemy
-#import datetime
-#import streamlit as st
-#import webbrowser
-#import time
+
 
 
 
 st.write("Hello")
-
-#PAGES = {
-    #"Aftertreatment_Claim_Coding ": app1,
-    #"CI_Claim_Coding": app2,
-    #"ECN_Tracking": app3,
-    #"D60": app4,
-    
-#} 
-#st.sidebar.title('Navigation')
-#selection = st.sidebar.radio("Go to", list(PAGES.keys()))
-#page = PAGES[selection]
-#page.app()
-
