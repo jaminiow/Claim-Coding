5bd00926786b34eff70a90eaeeaf9e6af5c1c7bb
jaminiow <75037391+jaminiow@users.noreply.github.com>
2020-12-11 13:35:08 -0600

    Add files via upload

---
+++ app1.py
@@ -0,0 +1,6 @@
+# app1.py
+import streamlit as st
+def app():
+    st.title('APP1')
+    st.markdown("<h1 style='text-align: left; color: red;'>Some title</h1>", unsafe_allow_html=True)
+    st.write('Welcome to app1')
