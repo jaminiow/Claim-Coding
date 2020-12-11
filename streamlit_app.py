c87d4792eee217642f218fec3dccef43268a09bc
jaminiow <75037391+jaminiow@users.noreply.github.com>
2020-12-11 15:23:19 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,23 @@
 #app.py
 import streamlit as st
+import app1
+import pyodbc as py  #Used for Database Connections
 st.set_page_config(layout="wide")
 
-"Hello"
+PAGES = {
+    "Aftertreatment_Claim_Coding ": app1,
+} 
+st.sidebar.title('Navigation')
+selection = st.sidebar.radio("Go to", list(PAGES.keys()))
+page = PAGES[selection]
+page.app()
 
-import pyodbc as py  #Used for Database Connections
+
+
+
+
+
+"Hello"
 
 #######################################################################
 # HANA Conenctions
