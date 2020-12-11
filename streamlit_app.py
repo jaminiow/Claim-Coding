1d070fab04e7550663cd663cb2891c353fb3d1c6
jaminiow <75037391+jaminiow@users.noreply.github.com>
2020-12-11 13:21:38 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,10 @@
 import streamlit as st
 st.set_page_config(layout="wide")
 
-import app1
-import app2
-import app3
-import app4
+#import app1
+#import app2
+#import app3
+#import app4
 import streamlit as st
 import numpy as np
 import pandas as pd  #Used for Dataframe Creations
