f7bb8002888edb5dbd4ad317408ad77f783124a7
jaminiow <75037391+jaminiow@users.noreply.github.com>
2020-12-11 12:30:34 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,30 @@
+#app.py
 import streamlit as st
+st.set_page_config(layout="wide")
 
-"Hello"
-st.balloons()
+import app1
+import app2
+import app3
+import app4
+import streamlit as st
+import numpy as np
+import pandas as pd  #Used for Dataframe Creations
+import pyodbc as py  #Used for Database Connections
+import openpyxl  # Used for Excel
+import sqlalchemy
+import datetime
+import streamlit as st
+import webbrowser
+import time
 
+PAGES = {
+    "Aftertreatment_Claim_Coding ": app1,
+    "CI_Claim_Coding": app2,
+    "ECN_Tracking": app3,
+    "D60": app4,
+    
+} 
+st.sidebar.title('Navigation')
+selection = st.sidebar.radio("Go to", list(PAGES.keys()))
+page = PAGES[selection]
+page.app()
