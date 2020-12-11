7e912da834c2eda50b02dfd710949c6c0dfb0d7e
jaminiow <75037391+jaminiow@users.noreply.github.com>
2020-12-11 13:55:31 -0600

    Update app1.py

--- app1.py
+++ app1.py
@@ -1,6 +1,6 @@
 # app1.py
 import streamlit as st
 def app():
-    st.title('Hi Yoshita')
+    st.title('Yoshita')
     st.markdown("<h1 style='text-align: left; color: red;'>Welcome to Streamlit</h1>", unsafe_allow_html=True)
     st.write('Welcome to app1')
