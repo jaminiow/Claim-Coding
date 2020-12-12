2e6520ecb5b576446721aee2f9bfc189c2bb594e
jaminiow <75037391+jaminiow@users.noreply.github.com>
2020-12-11 18:49:22 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,16 +22,3 @@ page.app()
 #######################################################################
 # HANA Conenctions
 #conn = py.connect(Trusted_Connection='yes', phn='HDBODBC',SERVERNODE='phnhdb.dx.deere.com:30015')
-
-#SQL Connections
-conn1 = py.connect('Driver={SQL Server};'
-                      'Server=FSDERGSQL1.SDE.DEERE.COM;'
-                      'Database=JDPS_941_SALES_DB;'
-                      'Trusted_Connection=yes;')
-
-#SQL Connections
-conn2 = py.connect('Driver={SQL Server};'
-                      'Server=wdx2ua3031cmp\sqlexpress;'
-                      'Database=JDPS_990_AFTERTREATMENT;'
-                      'Trusted_Connection=yes;')
-#######################################################################
