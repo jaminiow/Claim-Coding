6ab77f77436557418387a390081c96ed845c0a58
jaminiow <75037391+jaminiow@users.noreply.github.com>
2020-12-11 19:37:12 -0600

    Update app1.py

--- app1.py
+++ app1.py
@@ -4,3 +4,32 @@ def app():
     st.title('Yoshita')
     st.markdown("<h1 style='text-align: left; color: red;'>Welcome to Streamlit</h1>", unsafe_allow_html=True)
     st.write('Welcome to app1')
+
+    #Read in Data from CA_PARTNUMBER Table from Hana
+
+    def loadpartnumber(): # Hana: Run Query on CA_WORKFLOW_1
+        st.write('loadpartnumber')
+        HANA_PARTNUMBER_df = pd.read_sql('''
+
+            Select 
+            ECN AS PDMLINK_ECN,        
+            ECNSTATE AS PDMLINK_ECNSTATE,  
+            REVISION AS PDMLINK_REV,
+            PART_STATE AS PDMLINK_PARTSTATE,
+            DESIGNCONTROL AS PDMLINK_DesignControl,
+            PART_NUMBER as PNC
+    
+
+            From _SYS_BIC."deere.pdm.analytics.model/CA_PARTNUMBER"
+
+            WHERE
+            DESIGNCONTROL LIKE '%D0%'
+            AND
+            LATEST_REV_IND = 'X'
+
+         
+
+
+            '''
+            , conn)
+        return HANA_PARTNUMBER_df
