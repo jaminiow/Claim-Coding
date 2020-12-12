fa75f3bc235c0eb057a5109ac5796e123fdc4f4d
jaminiow <75037391+jaminiow@users.noreply.github.com>
2020-12-12 11:06:22 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 #app.py
 import streamlit as st
 import app1
-import pyodbc as py  #Used for Database Connections
+#import pyodbc as py  #Used for Database Connections
 st.set_page_config(layout="wide")
 
 PAGES = {
@@ -17,3 +17,13 @@ page.app()
 #conn = py.connect(Trusted_Connection='yes', DRIVER='SQL Server',SERVERNODE='phnhdb.dx.deere.com:30015',User='rg41159',Password='IHOPEIGETMYPHDIN13YEARS')
 
 #cnxn = py.connect('DRIVER="CData ODBC Driver for SAP HANA";User=system;Password="IHOPEIGETMYPHDIN13YEARS";SERVERNODE="phnhdb.dx.deere.com:30015"')
+import pyodbc 
+# Some other example server values are
+# server = 'localhost\sqlexpress' # for a named instance
+server = 30015
+server = 'phnhdb.dx.deere.com' 
+#database = 'phnhdb' 
+username = 'rg41159' 
+password = 'IHOPEIGETMYPHDIN13YEARS' 
+cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
+cursor = cnxn.cursor()
