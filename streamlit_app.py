d1400762376ff993a82b9b6107f81af51df7ce11
jaminiow <75037391+jaminiow@users.noreply.github.com>
2020-12-12 10:05:44 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,5 +16,5 @@ page.app()
 #SQL Connections
 conn1 = py.connect('Driver32=C:\WINDOWS\system32\SQLSRV32.dll' 
                       'Server=FSDERGSQL1.SDE.DEERE.COM;'
-                      'Database=JDPS_941_SALES_DB;'
+                      'Database=AFTERTREATMENT;'
                       'Trusted_Connection=yes;')
